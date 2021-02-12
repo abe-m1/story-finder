c1ae69d899e272f0b1eb28f8aca8d3f87f7125da
abe menashy <abestbet1@gmail.com>
2021-02-11 22:21:55 -0800

    click marker on map

--- utils/map.js
+++ utils/map.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from 'react';
 import {
   withScriptjs,
   withGoogleMap,
@@ -7,23 +8,47 @@ import {
 } from 'react-google-maps';
 
 const Map = withScriptjs(
-  withGoogleMap((props) => (
-    <GoogleMap defaultZoom={2} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      {props.isMarkerShown &&
-        props.userMarkers.map((marker) => {
-          console.log(marker.position);
-          return (
-            <Marker
-              position={{ lat: marker.position.lat, lng: marker.position.long }}
-            />
-          );
-        })}
+  withGoogleMap((props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleToggleOpen = () => {
+      setIsOpen(true);
+    };
 
-      {/* {props.isMarkerShown && (
+    const handleToggleClose = () => {
+      setIsOpen(false);
+    };
+    return (
+      <GoogleMap defaultZoom={2} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+        {props.isMarkerShown &&
+          props.userMarkers.map((marker, i) => {
+            console.log(marker.position);
+            return (
+              <Marker
+                key={i}
+                position={{
+                  lat: marker.position.lat,
+                  lng: marker.position.long,
+                }}
+                onClick={() => handleToggleOpen()}
+              >
+                {isOpen && (
+                  <InfoWindow onCloseClick={() => handleToggleClose()}>
+                    <>
+                      <img style={{ width: '40px' }} src={marker.userImage} />
+                      <p>{marker.userName}</p>
+                    </>
+                  </InfoWindow>
+                )}
+              </Marker>
+            );
+          })}
+
+        {/* {props.isMarkerShown && (
         <Marker position={{ lat: -30.397, lng: 110.644 }} />
       )} */}
-    </GoogleMap>
-  ))
+      </GoogleMap>
+    );
+  })
 );
 
 export default Map;
