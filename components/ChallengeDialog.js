208fca26c040b705254843a42e7232235f693f40
abe menashy <abestbet1@gmail.com>
2021-02-14 10:06:52 -0800

    challenge modal

---
+++ components/ChallengeDialog.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+import ChallengeForm from '../components/ChallengeForm';
+
+const styles = {
+  appBar: {
+    position: 'relative',
+  },
+  flex: {
+    flex: 1,
+  },
+  imgContainer: {
+    position: 'relative',
+    flex: 1,
+    padding: 16,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  img: {
+    maxWidth: '50%',
+    maxHeight: '50%',
+  },
+};
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
+
+class ImgDialog extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  onSuccessSubmit = (data) => {
+    this.props.onSuccessSubmit(data);
+  };
+
+  render() {
+    const { classes, userId, user } = this.props;
+    return (
+      <Dialog
+        // fullScreen
+        open={this.props.addConnection}
+        onClose={this.props.onClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              onClick={this.props.onClose}
+              aria-label="Close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.flex}
+            >
+              Submit Challenge
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {/* <div className={classes.imgContainer}>
+          <img src={this.props.img} alt="Cropped" className={classes.img} />
+        </div> */}
+        <ChallengeForm
+          user={user}
+          formId="new-connection-form"
+          userId={userId}
+          onSuccessSubmit={this.onSuccessSubmit}
+          onboardStep={4}
+          userForm={{ imagePreviewUrl: '' }}
+        />
+        {/* <Button onClick={this.props.onSelect}>Choose</Button> */}
+      </Dialog>
+    );
+  }
+}
+
+export default withStyles(styles)(ImgDialog);
--- pages/challenge/[id].js
+++ pages/challenge/[id].js
@@ -1,11 +1,17 @@
+import { useUser } from '../../lib/hooks';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import challenges from '../../public/data.json';
+import ChallengeDialog from '../../components/ChallengeDialog';
 import Layout from '../../components/layout';
 
 const Post = () => {
+  const user = useUser();
   const router = useRouter();
+
   const [currentChallenge, setCurrentChallenge] = useState({});
+  const [addConnection, setAddConnection] = useState(false);
+
   useEffect(() => {
     console.log(router.query);
     let challenge = challenges.find(
@@ -16,11 +22,26 @@ const Post = () => {
   });
   const { id } = router.query;
 
+  const onSuccessSubmit = () => {};
+
+  const onClose = useCallback(() => {}, []);
+
+  if (!user || user.isLoggedIn === false) {
+    return <Layout>Loading...</Layout>;
+  }
+
   return (
     <Layout>
       <p>Post: {id}</p>
       <p>{currentChallenge.challengeName}</p>
-      <button>Complete Challenge</button>
+      <button onClick={() => setAddConnection(true)}>Complete Challenge</button>
+      <ChallengeDialog
+        user={user}
+        userId={user._id}
+        onSuccessSubmit={onSuccessSubmit}
+        addConnection={addConnection}
+        onClose={onClose}
+      />
     </Layout>
   );
 };
