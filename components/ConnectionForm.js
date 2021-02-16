23ac5e1fec9c5003f6c9dd4c616af646ca270871
abe menashy <abestbet1@gmail.com>
2021-02-15 22:30:11 -0800

    fix import statement

--- components/ConnectionForm.js
+++ components/ConnectionForm.js
@@ -6,7 +6,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import { getOrientation } from 'get-orientation/browser';
-import ImgDialog from '../components/ImgDialog';
+import ImgDialog from '../components/imgDialog';
 import { getCroppedImg, getRotatedImage } from '../components/canvasUtils';
 import { styles } from '../components/styles';
 import { useRouter } from 'next/router';
